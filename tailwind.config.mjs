f237949e72f3e2e10f41b503cc97896064fa6675
Ashrahx <elfudanshi5@gmail.com>
2024-04-05 23:44:33 -0600

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+                blueprimary: '#404EED',
+				blackgray: '#23272A',
+				grayprimary: '#F6F6F6',
+				blurple: '#5865F2'
+            },
+			boxShadow: {
+				'dc': '0 8px 5px rgba(0,0,0,.2)',
+				'loginshadow': '0 2px 10px 0 hsl(0 calc( 1 * 0%) 0% / 0.2)',
+            },
+		},
+	},
+	plugins: [animations],
+}
